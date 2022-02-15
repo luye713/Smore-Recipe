12fdb12748351de623a9aeac06eca7e0fe2ba16b
Lu Ye <alison94713@gmail.com>
2022-02-14 23:22:47 -0500

    style updates.

--- main_app/views.py
+++ main_app/views.py
@@ -12,7 +12,7 @@ from .forms import TripForm
 from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
-from xhtml2pdf import pisa
+# from xhtml2pdf import pisa
 
 # HOME
 def home(request):
@@ -39,16 +39,15 @@ def signup(request):
 @login_required
 def trip_list(request):
     trips = Trip.objects.filter(user=request.user)
-    return render(request, 'main_app/trip_list.html', { 'trips': trips })
+    return render(request, 'trip_list.html', { 'trips': trips })
 
 @login_required
 def trip_create(request, recipe_id):
     trip_form = TripForm() 
-    return render(request, 'main_app/trip_form.html', { 
-        'trip_form': trip_form, 
+    return render(request, 'trip_form.html', { 
+        'trip_form': trip_form,
         'recipe_id': recipe_id
          })
-
 
 def trip_add(request, recipe_id):
     form = TripForm(request.POST)
@@ -56,17 +55,17 @@ def trip_add(request, recipe_id):
         new_trip = form.save(commit=False)
         new_trip.user_id = request.user.id
         new_trip.save()
-    if recipe_id == 0 or recipe_id is None:
+    if recipe_id == 0:
         return redirect('trip_detail', trip_id=new_trip.id)
     else:
-        return redirect('recipe_detail', recipe_id=recipe_id)
+        return redirect('recipe_save', trip_id=new_trip.id, recipe_id=recipe_id)
 
 
 @login_required
 def trip_detail(request, trip_id):
     trip = Trip.objects.filter(id=trip_id, user=request.user).first()
     equipments = Equipment.objects.all()
-    return render(request, 'main_app/trip_detail.html', { 'trip': trip, 'equipments': equipments })
+    return render(request, 'trip_detail.html', { 'trip': trip, 'equipments': equipments })
 
 def trip_delete(requst, trip_id):
     Trip.objects.get(id=trip_id).delete()
@@ -88,7 +87,7 @@ def recipe_list(request, category, equipment):
     categories = Category.objects.all()
     equipments = Equipment.objects.all()
 
-    return render(request, 'main_app/recipe_list.html', {
+    return render(request, 'recipe_list.html', {
         'categories': categories,
         'equipments': equipments,
         'recipes': recipes,
@@ -102,11 +101,13 @@ def recipe_detail(request, recipe_id):
     categories = Category.objects.filter(id__in = recipe.categories.all().values_list('id'))
     equipments = Equipment.objects.filter(id__in = recipe.equipments.all().values_list('id'))
     trips = Trip.objects.filter(user=request.user)
-    return render(request, 'main_app/recipe_detail.html', {
+    trips_have_recipe = Trip.objects.filter(recipes__id=recipe_id)
+    return render(request, 'recipe_detail.html', {
         'recipe': recipe,
         'categories': categories,
         'equipments': equipments,
         'trips': trips,
+        'trips_have_recipe': trips_have_recipe
     })
 
 
@@ -114,7 +115,7 @@ def recipe_detail(request, recipe_id):
 def recipe_choose(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
     trips = Trip.objects.filter(user=request.user).exclude(recipes=recipe)
-    return render(request, 'main_app/recipe_choose.html', { 'trips': trips, 'recipe': recipe })
+    return render(request, 'recipe_choose.html', { 'trips': trips, 'recipe': recipe })
 
 
 @login_required
